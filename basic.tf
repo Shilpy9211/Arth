6dbf80ad0a5ca4f2b753a49a2f557881387a2083
root <root@controller.node>
2021-10-03 16:43:30 -0400

    added playbook for http authentication

--- basic.tf
+++
@@ -1,8 +0,0 @@
-variable "x" {
-	type = string                                     #to define a variable
-	default = "shilpy"                               #if value is not coming from any source ,then this will be default value of x
-}
-
-output "op" {
-	value = "this is test code by ${var.x}"                 #to retrieve value of variable                      
-}
